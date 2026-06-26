6a2abb8d7b5e7f1bfe1b7919bbfa477d0e8804b2
usman harun <usmanharuna1738@gmail.com>
2026-06-26 23:31:10 +0100

    chore: hide rangers PIN

--- resources/views/livewire/rangers-dashboard.blade.php
+++ resources/views/livewire/rangers-dashboard.blade.php
@@ -178,7 +178,26 @@
                 <flux:input wire:model="editPhone" label="Phone Number" placeholder="+2347012345678" required />
                 <flux:input wire:model="editEmail" label="Email" placeholder="ranger@wildlife.gov.ng" />
                 <flux:input wire:model="editLocation" label="Base Location" placeholder="Kamuku National Park HQ" />
-                <flux:input wire:model="editPin" label="PIN" maxlength="4" placeholder="0000" required />
+
+                {{-- PIN with show/hide toggle --}}
+                <div x-data="{ showPin: false }" class="relative">
+                    <flux:field label="PIN">
+                        <div class="w-full relative block group/input" data-flux-input>
+                            <input wire:model="editPin"
+                                   type="password"
+                                   x-bind:type="showPin ? 'text' : 'password'"
+                                   maxlength="4"
+                                   placeholder="0000"
+                                   required
+                                   class="w-full border rounded-lg block appearance-none text-base sm:text-sm py-2 h-10 leading-[1.375rem] ps-3 pe-8 bg-white dark:bg-white/10 text-zinc-700 placeholder-zinc-400 dark:text-zinc-300 dark:placeholder-zinc-400 shadow-xs border-zinc-200 dark:border-white/10" />
+                            <button @click="showPin = !showPin" type="button"
+                                    class="absolute right-2 top-1/2 -translate-y-1/2 text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition-colors">
+                                <svg x-show="!showPin" xmlns="http://www.w3.org/2000/svg" class="size-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/><path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/></svg>
+                                <svg x-show="showPin" xmlns="http://www.w3.org/2000/svg" class="size-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"/></svg>
+                            </button>
+                        </div>
+                    </flux:field>
+                </div>
             </div>
 
             <div class="flex gap-2">
@@ -202,7 +221,26 @@
                 <flux:input wire:model="editPhone" label="Phone Number" placeholder="+2347012345678" required />
                 <flux:input wire:model="editEmail" label="Email" placeholder="ranger@wildlife.gov.ng" />
                 <flux:input wire:model="editLocation" label="Base Location" placeholder="Kamuku National Park HQ" />
-                <flux:input wire:model="editPin" label="PIN" maxlength="4" placeholder="0000" required />
+
+                {{-- PIN with show/hide toggle --}}
+                <div x-data="{ showPin: false }" class="relative">
+                    <flux:field label="PIN">
+                        <div class="w-full relative block group/input" data-flux-input>
+                            <input wire:model="editPin"
+                                   type="password"
+                                   x-bind:type="showPin ? 'text' : 'password'"
+                                   maxlength="4"
+                                   placeholder="0000"
+                                   required
+                                   class="w-full border rounded-lg block appearance-none text-base sm:text-sm py-2 h-10 leading-[1.375rem] ps-3 pe-8 bg-white dark:bg-white/10 text-zinc-700 placeholder-zinc-400 dark:text-zinc-300 dark:placeholder-zinc-400 shadow-xs border-zinc-200 dark:border-white/10" />
+                            <button @click="showPin = !showPin" type="button"
+                                    class="absolute right-2 top-1/2 -translate-y-1/2 text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition-colors">
+                                <svg x-show="!showPin" xmlns="http://www.w3.org/2000/svg" class="size-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/><path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/></svg>
+                                <svg x-show="showPin" xmlns="http://www.w3.org/2000/svg" class="size-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"/></svg>
+                            </button>
+                        </div>
+                    </flux:field>
+                </div>
             </div>
 
             <div class="flex gap-2">
